fc430c09ae45a98dbaa5a3e3962336ed7d9a9487
Timothy Wang <timqwang@gmail.com>
2019-09-28 19:37:53 -0700

    Add files via upload

---
+++ lab1/HelloUser2.java
@@ -0,0 +1,10 @@
+// Another simple Hello User Program, HelloUser2.java
+// By: Timmy Wang, tqwang 12B
+
+class HelloUser2 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the program!");
+		System.out.println("I will be your guide, Timmy.");
+		System.out.println("Nah, nevermind, goodbye :)");
+	}
+}
--- lab1/README.txt
+++ lab1/README.txt
@@ -1,1 +1,9 @@
+Files being submitted:
 
+HelloUser.java
+HelloUser2.java
+Makefile1
+Makefile2
+Makefile3
+README.txt
+Hello
